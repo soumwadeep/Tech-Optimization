1695a8dd0a4c4245c4417e1a890d80b7100f07a2
Soumwadeep Guha <soumwadeep@gmail.com>
2022-06-02 11:16:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Home from './Home'
+import About from './About'
+import Service from './Service'
+import Contact from './Contact'
+import Navbar from './Navbar'
+import { Routes, Route , Navigate} from 'react-router-dom'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element ={<Home/>}/>
+        <Route exact path="/about" element ={<About/>}/>
+        <Route exact path="/service" element ={<Service/>}/>
+        <Route exact path="/contact" element ={<Contact/>}/>
+        <Route path="*" element={<Navigate to="/" replace />}/>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
